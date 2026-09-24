7789facbef2da3dce1fe5bca5329cd8668725797
Sergei Grichine <msghub@hotmail.com>
2026-09-24 12:52:50 -0500

    Update image_to_depth_yolo_node.launch.py

--- launch/image_to_depth_yolo_node.launch.py
+++ launch/image_to_depth_yolo_node.launch.py
@@ -23,6 +23,7 @@ Launch it:
 """
 
 import os
+from math import pi
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
@@ -119,33 +120,33 @@ def generate_launch_description():
     #  for RViz2 visualization. The following two static transform publishers are for that purpose.
     # See https://github.com/slgrobotics/huskylens2_ros2/blob/main/README.md#the-optical-coordinate-system-for-cameras-and-sensors
 
-    # static transform publisher "map->camera_3d_link" for RViz2:
+    # static transform publisher "camera_3d_link->map" for RViz2:
     tf_camera_to_map = Node(package = "tf2_ros", 
                     executable = "static_transform_publisher",
                     arguments=[
                         '--x', '5.0',     # X translation in meters
                         '--y', '0.0',     # Y translation in meters
                         '--z', '0.57',    # Z translation in meters (camera height above ground)
-                        '--roll',  '3.14159265359',  # Roll in radians
+                        '--roll',  '0.0', # Roll in radians
                         '--pitch', '0.0', # Pitch in radians
-                        '--yaw',   '3.14159265359',   # Yaw in radians (e.g., 1.57079632679 = 90 degrees)
+                        '--yaw',   '0.0', # Yaw in radians (e.g., 1.57079632679 = 90 degrees)
                         '--frame-id', 'map', # Parent frame ID
                         '--child-frame-id', 'camera_3d_link' # Child frame ID
                     ]
     )
 
 
-    # static transform publisher "map->camera_3d_link_optical" for RViz2:
+    # static transform publisher "camera_3d_link_optical->camera_3d_link" for RViz2:
     tf_camera_optical_to_map = Node(package = "tf2_ros", 
                     executable = "static_transform_publisher",
                     arguments=[
-                        '--x', '5.0',     # X translation in meters
+                        '--x', '0.0',     # X translation in meters
                         '--y', '0.0',     # Y translation in meters
-                        '--z', '0.57',    # Z translation in meters (camera height above ground)
-                        '--roll', '-1.57079632679',  # Roll in radians
+                        '--z', '0.0',     # Z translation in meters (camera height above ground)
+                        '--roll', str(pi / 2),  # Roll in radians
                         '--pitch', '0.0', # Pitch in radians
-                        '--yaw', '1.57079632679',   # Yaw in radians (e.g., 1.57079632679 = 90 degrees)
-                        '--frame-id', 'map', # Parent frame ID
+                        '--yaw', str(pi / 2),   # Yaw in radians (90 degrees)
+                        '--frame-id', 'camera_3d_link', # Parent frame ID
                         '--child-frame-id', 'camera_3d_link_optical' # Child frame ID
                     ]
     )
